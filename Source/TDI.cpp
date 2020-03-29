635b068e0335a7394f57d445cebaefaa0428f010
root <jps105@inlumine.ual.es>
2020-03-29 15:21:47 +0200

    Algoritmo version incial

--- Source/TDI.cpp
+++ Source/TDI.cpp
@@ -1,19 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>  
 #include <iostream>
-#include <algorithm>
 #include <vector>
 #include <map>
 
 #include <math.h>
-#include <fcntl.h>
-
-#include <time.h>
 
 #include <C_General.hpp>
-#include <C_Trace.hpp>
-#include <C_File.hpp>
-#include <C_Arguments.hpp>
 #include <C_Matrix.hpp>
 #include <C_Image.hpp>
 
@@ -21,27 +14,50 @@ int Test(int argc, char **argv);
 int GREY_SCALE = 256;
 C_Matrix mat(0, 0, 0, 0, 0);
 typedef long IndexT;
+typedef double ElementT;
 
-/*Historigrama futura implementacion del calculo de valor humbral optimo*/
-int Get_Historigrama(unsigned int* historigrama, C_Image a) {
+int Get_Limite(C_Image a) {
+	int histograma[256] = {};
 	IndexT row, col;
-	int sum = 0;
-	float varianza = 0.0;
-	float dtipica = 0.0;
 
 	/*Definicion del historigrama*/
-	for (int i = 0; i < GREY_SCALE; i++) historigrama[i] = 0;
+	for (int i = 0; i < GREY_SCALE; i++) histograma[i] = 0;
 
 	for (row = a.FirstRow(); row <= a.LastRow(); row++)
 		for (col = a.FirstCol(); col <= a.LastCol(); col++)
-			historigrama[(int)a(row, col)]++;
-	
-	return 0;
+			histograma[(int)a(row, col)]++;
+
+	//Busqueda de punto de equilibrio de histograma
+	//Pendiente de implementacion algoritmo de OTSU, posible mejora
+	int medio = 128;
+	int pesoIz = histograma[0];
+	int pesoDe = histograma[255];
+	int ini = 0;
+	int fin = 255;
+
+	while (ini <= fin) {
+		if (pesoDe > pesoIz) {
+			pesoDe -= histograma[fin--];
+			if (((ini + fin) / 2) < medio) {
+				pesoDe += histograma[medio];
+				pesoIz -= histograma[medio--];
+			}
+		}
+		else if (pesoIz >= pesoDe) {
+			pesoIz -= histograma[ini++];
+			if (((ini + fin) / 2) >= medio) {
+				pesoIz += histograma[medio + 1];
+				pesoDe -= histograma[medio + 1];
+				medio++;
+			}
+		}
+	}
+	return medio;
 }
 
 /*Calculo de los puntos minimos de la imagen*/
-std::map<long, std::map<IndexT,IndexT>> Get_Minimos(C_Image a) {
-	std::map<long, std::map<IndexT, IndexT>> map;
+std::map<ElementT, std::map<IndexT,IndexT>> Get_Minimos(C_Image a) {
+	std::map<ElementT, std::map<IndexT, IndexT>> map;
 	IndexT row, col;
 
 	for (row = a.FirstRow(); row <= a.LastRow(); row++)
@@ -51,58 +67,63 @@ std::map<long, std::map<IndexT,IndexT>> Get_Minimos(C_Image a) {
 	return map;
 }
 
-void Flood(C_Image a, IndexT x, IndexT y, float vasija) {
-	IndexT row, col;
-	auto aux = 0;
-	auto limite = 100;
-
-	if (!mat(x, y) > 0) {
+/*Algoritmo de llenado, version lenta sin limites automaticos ni marcadores
+Posible mejora, introduccion de hilos para agilizar el calculo de forma paralela*/
+void Flood(C_Image a, IndexT x, IndexT y, ElementT limite) {
+	if (mat.In(x,y) && !(mat(x, y) > 0)) {
+		IndexT row, col;
+		C_Matrix::ElementT aux = 0;
 		//SE
 		for (row = x; row <= a.LastRow() - 1; row++)
 			for (col = y; col <= a.LastCol() - 1; col++) {
+				if (!mat.In(x - 1, y - 1)) break;
 				aux = a(row - 1, col - 1);
-				
-				if (a(row, col) - aux > 30) {
+				if (a(row, col) - aux > limite) {
 					mat(row, col) = 255;
 					break;
 				}
-
-				if (a(row, col) - vasija <= limite) mat(row, col) = 0;
 			}
-
+		
+		for (row = x; row <= a.LastRow() - 1; row++)
+			for (col = y; col <= a.LastCol() - 1; col++) {
+				if (!mat.In(x - 1, y - 1)) break;
+				aux = a(row - 1, col - 1);
+				if (a(row, col) - aux > limite) {
+					mat(row, col) = 255;
+					break;
+				}
+			}
+		
 		//SW
 		for (row = x; row <= a.LastRow() - 1; row++)
 			for (col = y; col >= a.FirstCol() + 1; col--) {
+				if (!mat.In(x - 1, y + 1)) break;
 				aux = a(row - 1, col + 1);
-				
-				if (a(row, col) - aux > 30) {
+				if (a(row, col) - aux > limite) {
 					mat(row, col) = 255;
 					break;
 				}
-				if (a(row, col) - vasija <= limite) mat(row, col) = 0;
 			}
 
 		//NE
-		for (row = x; row >= a.FirstCol() + 1; row--)
+		for (row = x; row >= a.FirstRow() + 1; row--)
 			for (col = y; col <= a.LastCol() - 1; col++) {
+				if (!mat.In(x + 1, y - 1)) break;
 				aux = a(row + 1, col - 1);
-				
-				if (a(row, col) - aux > 30) {
+				if (a(row, col) - aux > limite) {
 					mat(row, col) = 255;
 					break;
 				}
-				if (a(row, col) - vasija <= limite) mat(row, col) = 0;
 			}
 		//NW
-		for (row = x; row >= a.FirstCol() + 1; row--)
+		for (row = x; row >= a.FirstRow() + 1; row--)
 			for (col = y; col >= a.FirstCol() + 1; col--) {
+				if (!mat.In(x + 1, y + 1)) break;
 				aux = a(row + 1, col + 1);
-				
-				if (a(row, col) - aux > 30) {
+				if (a(row, col) - aux > limite) {
 					mat(row, col) = 255;
 					break;
 				}
-				if (a(row, col) - vasija <= limite) mat(row, col) = 0;
 			}
 	}
 }
@@ -111,26 +132,35 @@ int main(int argc, char **argv)
 {
 	IndexT row, col;
 	C_Image a;
-	std::map<long, std::map<IndexT, IndexT>> map;
-
+	C_Image destino;
+	std::map<ElementT, std::map<IndexT, IndexT>> map;
 	auto aux = 0;
 
-	a.ReadBMP("MisEjemplos/Hercules.bmp");
+	a.ReadBMP("MisEjemplos/Aguadulce_Gris.bmp");
 	a.Grey();
 
+	//Ajustar limite || REVISAR
+	aux = Get_Limite(a);
+	
+	//Binarizacion de la imagen
+	for (row = a.FirstRow(); row <= a.LastRow(); row++)
+		for (col = a.FirstCol(); col <= a.LastCol(); col++)
+			if (a(row, col) > aux) a(row, col) = 255;
+			else a(row, col) = 0;
+
+	//CALCULO DE PUNTOS MINIMOS MEDIANTE FIFO
 	map = Get_Minimos(a);
 	mat.Resize(a.FirstRow(),a.LastRow(),a.FirstCol(),a.LastCol(),0);
-
+	
+	//METODO DE LLENADO
 	for (auto x : map) {
-		if (x.first > 100) break;
 		for (auto y : x.second) {
-			Flood(a, y.first, y.second, x.first);
+			Flood(a, y.first, y.second, 10);
 		}
 	}
 
 	C_Image b(mat);
-
-	b.WriteBMP("MisEjemplos/Hercules1.bmp");
+	b.WriteBMP("MisEjemplos/Aguadulce_Gris1.bmp");
 
 	return 0;
 }
