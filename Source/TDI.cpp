07838e4d09444950198147f3c3545fc52efa88a5
root <jps105@inlumine.ual.es>
2020-03-29 18:01:02 +0200

    Watershed ahora con Hilos

--- Source/TDI.cpp
+++ Source/TDI.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <vector>
 #include <map>
+#include <thread>
 
 #include <math.h>
 
@@ -13,10 +14,11 @@
 int Test(int argc, char **argv);
 int GREY_SCALE = 256;
 C_Matrix mat(0, 0, 0, 0, 0);
+C_Image a;
 typedef long IndexT;
 typedef double ElementT;
 
-int Get_Limite(C_Image a) {
+int Get_Limite() {
 	int histograma[256] = {};
 	IndexT row, col;
 
@@ -56,8 +58,8 @@ int Get_Limite(C_Image a) {
 }
 
 /*Calculo de los puntos minimos de la imagen*/
-std::map<ElementT, std::map<IndexT,IndexT>> Get_Minimos(C_Image a) {
-	std::map<ElementT, std::map<IndexT, IndexT>> map;
+map<ElementT, map<IndexT,IndexT>> Get_Minimos(C_Image a) {
+	map<ElementT, map<IndexT, IndexT>> map;
 	IndexT row, col;
 
 	for (row = a.FirstRow(); row <= a.LastRow(); row++)
@@ -67,37 +69,47 @@ std::map<ElementT, std::map<IndexT,IndexT>> Get_Minimos(C_Image a) {
 	return map;
 }
 
+void Binarizacion() {
+	IndexT row, col;
+	auto aux = 0;
+	//Ajustar limite || REVISAR
+	aux = Get_Limite();
+
+	//Binarizacion de la imagen
+	for (row = a.FirstRow(); row <= a.LastRow(); row++)
+		for (col = a.FirstCol(); col <= a.LastCol(); col++)
+			if (a(row, col) > aux) a(row, col) = 255;
+			else a(row, col) = 0;
+
+}
+
 /*Algoritmo de llenado, version lenta sin limites automaticos ni marcadores
 Posible mejora, introduccion de hilos para agilizar el calculo de forma paralela*/
-void Flood(C_Image a, IndexT x, IndexT y, ElementT limite) {
+
+void FloodHilos(double x, double y, double limite, int numHilo, bool hilos = false) {
 	if (mat.In(x,y) && !(mat(x, y) > 0)) {
 		IndexT row, col;
-		C_Matrix::ElementT aux = 0;
+		ElementT aux = 0;
+
 		//SE
+		if(numHilo == 0 || !hilos)
 		for (row = x; row <= a.LastRow() - 1; row++)
 			for (col = y; col <= a.LastCol() - 1; col++) {
 				if (!mat.In(x - 1, y - 1)) break;
+
 				aux = a(row - 1, col - 1);
 				if (a(row, col) - aux > limite) {
 					mat(row, col) = 255;
 					break;
 				}
 			}
-		
-		for (row = x; row <= a.LastRow() - 1; row++)
-			for (col = y; col <= a.LastCol() - 1; col++) {
-				if (!mat.In(x - 1, y - 1)) break;
-				aux = a(row - 1, col - 1);
-				if (a(row, col) - aux > limite) {
-					mat(row, col) = 255;
-					break;
-				}
-			}
-		
+
 		//SW
+		if (numHilo == 1 || !hilos)
 		for (row = x; row <= a.LastRow() - 1; row++)
 			for (col = y; col >= a.FirstCol() + 1; col--) {
 				if (!mat.In(x - 1, y + 1)) break;
+				
 				aux = a(row - 1, col + 1);
 				if (a(row, col) - aux > limite) {
 					mat(row, col) = 255;
@@ -106,9 +118,11 @@ void Flood(C_Image a, IndexT x, IndexT y, ElementT limite) {
 			}
 
 		//NE
+		if (numHilo == 2 || !hilos)
 		for (row = x; row >= a.FirstRow() + 1; row--)
 			for (col = y; col <= a.LastCol() - 1; col++) {
 				if (!mat.In(x + 1, y - 1)) break;
+				
 				aux = a(row + 1, col - 1);
 				if (a(row, col) - aux > limite) {
 					mat(row, col) = 255;
@@ -116,9 +130,11 @@ void Flood(C_Image a, IndexT x, IndexT y, ElementT limite) {
 				}
 			}
 		//NW
+		if (numHilo == 3 || !hilos)
 		for (row = x; row >= a.FirstRow() + 1; row--)
 			for (col = y; col >= a.FirstCol() + 1; col--) {
 				if (!mat.In(x + 1, y + 1)) break;
+				
 				aux = a(row + 1, col + 1);
 				if (a(row, col) - aux > limite) {
 					mat(row, col) = 255;
@@ -128,39 +144,45 @@ void Flood(C_Image a, IndexT x, IndexT y, ElementT limite) {
 	}
 }
 
+void Flood(bool hilos) {
+	thread hilo0, hilo1, hilo2, hilo3;
+	map<ElementT, std::map<IndexT, IndexT>> map;
+
+	Binarizacion();
+	map = Get_Minimos(a);
+
+	//METODO DE LLENADO HILOS REVISAR WARNINGS DE CONVERSION DE TIPO
+	if (hilos)
+		for (auto x : map) {
+			for (auto y : x.second) {
+				hilo0 = thread(FloodHilos, y.first, y.second, 10, 0, true);
+				hilo1 = thread(FloodHilos, y.first, y.second, 10, 1, true);
+				hilo2 = thread(FloodHilos, y.first, y.second, 10, 2, true);
+				hilo3 = thread(FloodHilos, y.first, y.second, 10, 3, true);
+
+				hilo0.join();
+				hilo1.join();
+				hilo2.join();
+				hilo3.join();
+			}
+		}
+
+	else
+		for (auto x : map)
+			for (auto y : x.second)
+				FloodHilos(y.first, y.second, 10, -1);
+}
+
 int main(int argc, char **argv)
 {
-	IndexT row, col;
-	C_Image a;
-	C_Image destino;
-	std::map<ElementT, std::map<IndexT, IndexT>> map;
-	auto aux = 0;
-
-	a.ReadBMP("MisEjemplos/Aguadulce_Gris.bmp");
+	a.ReadBMP("MisEjemplos/Ajedrez_Gris.bmp");
+	mat.Resize(a.FirstRow(),a.LastRow(),a.FirstCol(),a.LastCol(),0);
 	a.Grey();
 
-	//Ajustar limite || REVISAR
-	aux = Get_Limite(a);
+	Flood(false);
 	
-	//Binarizacion de la imagen
-	for (row = a.FirstRow(); row <= a.LastRow(); row++)
-		for (col = a.FirstCol(); col <= a.LastCol(); col++)
-			if (a(row, col) > aux) a(row, col) = 255;
-			else a(row, col) = 0;
-
-	//CALCULO DE PUNTOS MINIMOS MEDIANTE FIFO
-	map = Get_Minimos(a);
-	mat.Resize(a.FirstRow(),a.LastRow(),a.FirstCol(),a.LastCol(),0);
-	
-	//METODO DE LLENADO
-	for (auto x : map) {
-		for (auto y : x.second) {
-			Flood(a, y.first, y.second, 10);
-		}
-	}
-
 	C_Image b(mat);
-	b.WriteBMP("MisEjemplos/Aguadulce_Gris1.bmp");
+	b.WriteBMP("MisEjemplos/Ajedrez_Gris1.bmp");
 
 	return 0;
 }
