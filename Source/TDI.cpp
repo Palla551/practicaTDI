6ed30b33620216a6cb01abe2de9e5c3590702582
root <jps105@inlumine.ual.es>
2020-04-22 18:08:11 +0200

    WaterSeeds funciona

--- Source/TDI.cpp
+++ Source/TDI.cpp
@@ -19,6 +19,12 @@ C_Matrix a_sobel;
 typedef long IndexT;
 typedef double ElementT;
 
+struct Point{
+	IndexT x = -1;
+	IndexT y = -1;
+	double sob;
+};
+
 int Get_Limite() {
 	int histograma[256] = {};
 	IndexT row, col;
@@ -79,16 +85,69 @@ void Sobel(int limite) {
 	a.Free();
 }
 
-void FloodVecinos(IndexT x, IndexT y, long vecino, long seeds, long color) {
+void FloodFillRecursivo(IndexT x, IndexT y, long vecino, long seeds, long color) {
 	if (img.In(x, y)) {
 		if (img(x, y) != color) vecino--;
 		if (img(x, y) == color)
 		{
 			img(x, y) = seeds;
-			FloodVecinos(x + 1, y, vecino, seeds, color);
-			FloodVecinos(x, y + 1, vecino, seeds, color);
-			FloodVecinos(x - 1, y, vecino, seeds, color);
-			FloodVecinos(x, y - 1, vecino, seeds, color);
+			FloodFillRecursivo(x + 1, y, vecino, seeds, color);
+			FloodFillRecursivo(x, y + 1, vecino, seeds, color);
+			FloodFillRecursivo(x - 1, y, vecino, seeds, color);
+			FloodFillRecursivo(x, y - 1, vecino, seeds, color);
+		}
+	}
+}
+
+void FloodFill(IndexT x, IndexT y, long vecino, long seeds, long color)
+{
+	vector<Point> queue;
+	auto p = Point();
+	auto check = Point();
+	p.x = x;
+	p.y = y;
+
+	queue.push_back(p);
+
+	while (!queue.empty())
+	{
+		Point filled = queue.back();
+		queue.pop_back();
+
+		// Left
+		check.x = filled.x - 1;
+		check.y = filled.y;
+		if (img.In(check.x, check.y) && img(check.x, check.y) == color)
+		{
+			img(check.x, check.y) = seeds;
+			queue.push_back(check);
+		}
+
+		// Right
+		check.x = filled.x + 1;
+		check.y = filled.y;
+		if (img.In(check.x, check.y) && img(check.x, check.y) == color)
+		{
+			img(check.x, check.y) = seeds;
+			queue.push_back(check);
+		}
+
+		// Top
+		check.x = filled.x;
+		check.y = filled.y + 1;
+		if (img.In(check.x, check.y) && img(check.x, check.y) == color)
+		{
+			img(check.x, check.y) = seeds;
+			queue.push_back(check);
+		}
+
+		// Bot
+		check.x = filled.x;
+		check.y = filled.y - 1;
+		if (img.In(check.x, check.y) && img(check.x, check.y) == color)
+		{
+			img(check.x, check.y) = seeds;
+			queue.push_back(check);
 		}
 	}
 }
@@ -104,7 +163,7 @@ void Seeds(long umbral, long vecino,long seeds, long color) {
 	for (row = img.FirstRow(); row <= img.LastRow(); row++)
 		for (col = img.FirstCol(); col <= img.LastCol(); col++) {
 			if (img(row, col) == color) {
-				FloodVecinos(row, col, vecino, a_seeds, color);
+				FloodFill(row, col, vecino, a_seeds, color);
 				a_seeds++;
 				if (a_seeds >= 255) a_seeds = seeds;
 			}
@@ -113,7 +172,7 @@ void Seeds(long umbral, long vecino,long seeds, long color) {
 	
 }
 
-void WaterShed(long umbral, long vecino, long seeds = 10, long color = 255) {
+void WaterShed(long umbral, long vecino = 0, long seeds = 10, long color = 255) {
 	IndexT row, col;
 
 	//Primera parte, generacion de semillas
@@ -121,59 +180,61 @@ void WaterShed(long umbral, long vecino, long seeds = 10, long color = 255) {
 	Seeds(umbral, vecino, seeds, color);
 
 	//Segunda parte, inundacion
-	auto limite = umbral;
+	//auto limite = umbral;
+	//auto aux = 0;
+	//Point pos = Point();
 
-	while (limite > 0) {
-		for (row = img.FirstRow() + 1; row <= img.LastRow() - 1; row++)
-			for (col = img.FirstCol() + 1; col <= img.LastCol() - 1; col++) {
-				if (a_sobel(row, col) < limite) continue;
+	//while (limite > 0) {
+	//	for (row = img.FirstRow() + 1; row <= img.LastRow() - 1; row++)
+	//		for (col = img.FirstCol() + 1; col <= img.LastCol() - 1; col++) {
+	//			if (a_sobel(row, col) < limite) continue;
 
-				if (img(row + 1, col) > 0) {
-					img(row, col) = img(row + 1, col);
-					continue;
-				}
-				if (img(row, col + 1) > 0) {
-					img(row, col) = img(row, col + 1);
-					continue;
-				}
-				if (img(row + 1, col + 1) > 0) {
-					img(row, col) = img(row + 1, col + 1);
-					continue;
-				}
-				if (img(row - 1, col) > 0) {
-					img(row, col) = img(row - 1, col);
-					continue;
-				}
-				if (img(row, col - 1) > 0) {
-					img(row, col) = img(row, col - 1);
-					continue;
-				}
-				if (img(row - 1, col - 1) > 0) {
-					img(row, col) = img(row - 1, col - 1);
-				}
-			}
-		limite--;
-	}
-
-	//Tercera Parte, postprocesado
-/*
-	for (row = img.FirstRow(); row <= img.LastRow(); row++)
-		for (col = img.FirstCol(); col <= img.LastCol(); col++) {
-			if (a_sobel(row, col) < umbral) img(row, col) = color;
-		}
-		*/
+	//			if (img(row + 1, col) > 0) {
+	//				img(row, col) = img(row + 1, col);
+	//				continue;
+	//			}
+	//			if (img(row, col + 1) > 0) {
+	//				img(row, col) = img(row, col + 1);
+	//				continue;
+	//			}				
+	//			if (img(row + 1, col + 1) > 0) {
+	//				img(row, col) = img(row + 1, col + 1);
+	//				continue;
+	//			}
+	//			if (img(row - 1, col) > 0) {
+	//				img(row, col) = img(row - 1, col);
+	//				continue;
+	//			}
+	//			if (img(row, col - 1) > 0) {
+	//				img(row, col) = img(row, col - 1);
+	//				continue;
+	//			}
+	//			if (img(row - 1, col - 1) > 0) {
+	//				img(row, col) = img(row - 1, col - 1);
+	//				continue;
+	//			}
+	//			if (img(row + 1, col - 1) > 0) {
+	//				img(row, col) = img(row - 1, col - 1);
+	//				continue;
+	//			}
+	//			if (img(row - 1, col + 1) > 0) {
+	//				img(row, col) = img(row, col + 1);
+	//			}
+	//		}
+	//	limite--;
+	//}
 }
 
 int main(int argc, char **argv)
 {
-	a.ReadBMP("MisEjemplos/water_coins.bmp");
+	a.ReadBMP("MisEjemplos/Aguadulce_Gris.bmp");
 
 	img.Resize(a.FirstRow(),a.LastRow(),a.FirstCol(),a.LastCol(),0);
 
 	a.Grey();
 	a.MedianFilter(a, 3);
 
-	WaterShed(1, 4);
+	WaterShed(20);
 
 	img.palette.Read("PaletaSurtida256.txt");
 
